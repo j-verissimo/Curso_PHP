5d237ea3355ab88422d1f468eff879311130b17b
Jose Verissimo <verissimo.neb@gmail.com>
2021-12-04 12:26:32 -0300

    subindo questao 7 do exercicio de progressao

--- questao_7.php
+++ questao_7.php
@@ -3,7 +3,7 @@
      $numero = array(10,21,5,95,33);
      #Inicia a variavel que vai receber o valor de comparação. Iniciamos com a primeira array.
      $maior=$numero[0];
-     #Inicio das comparações
+     #Inicio das comparações / processamento
       if ($maior<$numero[1]) {
              $maior=$numero[1];
       }
@@ -17,10 +17,5 @@
       if ($maior<$numero[4]) {
              $maior=$numero[4];
       }
-
+      // Mostra na tela o resultado da comparação
       echo "Dentre os números $numero[0], $numero[1], $numero[2], $numero[3] e $numero[4] o maior deles é o $maior";
-
-
- /*$media=$soma_numeros/10;
- echo "A média dos 10 números criados é igual a $media";
-?>
